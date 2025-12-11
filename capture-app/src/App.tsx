45b043c1fb647c97438dd1af0e84afb1855a56f2
Mitchell Maber <mitchell.maber@entelect.co.za>
2025-12-11 15:46:04 +0200

    add mui tabs component

--- capture-app/src/App.tsx
+++ capture-app/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css"
 import TeammateSelector from "./components/teammate-selector/teammate-selector"
+import TabsCollection from "./components/tab-collection/tab-collection"
 
 function App() {
   return (
     <>
       <TeammateSelector />
+      <TabsCollection />
     </>
   )
 }
