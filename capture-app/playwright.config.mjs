ead4ea1df34cdc1358561df132a3e86abc1a5550
Nigel Gavin <nigel.gavin@entelect.co.za>
2025-12-17 12:51:40 +0200

    Add Playwright and initial tests

---
+++ capture-app/playwright.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  use: {
+    baseURL: 'http://localhost:5173',
+    headless: false,
+  },
+
+   webServer: {
+    command: 'npm run dev',
+    port: 5173,
+    timeout: 120 * 1000,
+    reuseExistingServer: true,
+  }
+});
+
