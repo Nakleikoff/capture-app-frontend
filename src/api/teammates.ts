c6ce8944cc41ca8c62b12d2942c86ae324f440c5
Mitchell Maber <mitchell.maber@entelect.co.za>
2025-12-18 11:28:05 +0200

    add and implement teammate calls

--- src/api/teammates.ts
+++ src/api/teammates.ts
@@ -13,37 +13,66 @@ const delay = (ms: number) => {
 }
 
 export async function getTeammates(): Promise<TeammatesResponse> {
-  const teammatesResponse = await fetch(
-    `${import.meta.env.VITE_API_URL}/teammates`
-  )
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/teammates`)
 
-  if (!teammatesResponse.ok) {
+  if (!response.ok) {
     return {
       success: false,
       data: [],
     }
   }
 
-  const { success, data } = await teammatesResponse.json()
-  return { success, data }
+  const { success, data } = await response.json()
+
+  const dummyData: TeammatesResponse = {
+    success: true,
+    data: [
+      {
+        id: 1,
+        name: "Mitchell",
+      },
+      {
+        id: 2,
+        name: "Luke",
+      },
+      {
+        id: 3,
+        name: "Lesego",
+      },
+      {
+        id: 4,
+        name: "Nigel",
+      },
+      {
+        id: 5,
+        name: "Saxon",
+      },
+      {
+        id: 6,
+        name: "Alexey",
+      },
+    ],
+  }
+  return {
+    success,
+    data,
+  }
 }
 
 export type CreateTeammateRequest = {
   name: string
 }
 export async function createTeammate(request: CreateTeammateRequest) {
-  const createTeammateResponse = await fetch(
-    `${import.meta.env.VITE_API_URL}/teammates`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(request),
-    }
-  )
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/teammates`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(request),
+  })
 
-  if (!createTeammateResponse.ok) {
+  if (!response.ok) {
+    console.error(response)
     return {
       success: false,
     }
