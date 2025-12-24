99c9d0db3d77a67bd9fc9ebc4ca9a9b551bc3c96
Alexey Maleykov <alexey.maleykov@mtn.co.za>
2025-12-24 12:34:34 +0200

    Update models

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import './App.scss';
 import TeammateSelector from './components/TeammateSelector/TeammateSelector';
 import FeedbackForm from './components/FeedbackForm/FeedbackForm';
 import { useState } from 'react';
+import type { Teammate } from './api/teammates';
 
 function App() {
-  const [teammateId, setTeammateId] = useState<number>(0);
+  const [teammate, setTeammate] = useState<Teammate>();
 
   return (
     <>
-      <TeammateSelector setTeammateId={setTeammateId} />
-      <FeedbackForm teammateId={teammateId} />
+      <TeammateSelector setTeammate={setTeammate} />
+      {teammate && <FeedbackForm teammate={teammate} />}
     </>
   );
 }
