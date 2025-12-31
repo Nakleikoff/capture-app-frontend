6eca6db185ad7cee724c59cb8d1831b82b35afc2
Mitchell Maber <mitchell.maber@entelect.co.za>
2025-12-31 10:45:15 +0200

    re-strucutre error handling in api-service

--- src/api/api-service.ts
+++ src/api/api-service.ts
@@ -1,39 +1,42 @@
 export type ApiResponse<T> =
   | { success: true; data: T }
-  | { success: false; error: { message: string } };
+  | { success: false; error: { code: string | number; message: string } };
 
 export async function apiRequest<RES, REQ = undefined>(
   input: RequestInfo,
   body?: REQ,
   method: 'GET' | 'POST' | 'PUT' | 'DELETE' = 'GET',
 ): Promise<ApiResponse<RES>> {
+  const token = import.meta.env.VITE_API_JWT_TOKEN;
   const headers: Record<string, string> = {
     'Content-Type': 'application/json',
     Accept: 'application/json',
-    Authorization: `Bearer ${import.meta.env.VITE_API_JWT_TOKEN}`,
+    Authorization: `Bearer ${token}`,
   };
 
-  const initialize = {
-    method,
-    headers,
-    body: body ? JSON.stringify(body) : undefined,
-  };
-  const response = await fetch(
-    `${import.meta.env.VITE_API_URL}/${input}`,
-    initialize,
-  );
+  try {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/${input}`, {
+      method,
+      headers,
+      body: body ? JSON.stringify(body) : undefined,
+    });
 
-  if (!response.ok) {
-    const { success, error } = await response.json();
-    const errorMessage = error || 'An unexpected error occurred.';
+    if (!response.ok) {
+      const { success, error } = await response.json();
+      const errorMessage =
+        error.details.message || 'An unexpected error occurred.';
 
-    return {
-      success,
-      error: errorMessage,
-    };
+      return {
+        success,
+        error: { code: error.code, message: errorMessage },
+      };
+    }
+
+    const { success, data } = await response.json();
+
+    return { success, data };
+  } catch (error: any) {
+    const message = error?.message ?? 'Network error';
+    return { success: false, error: { code: 'NETWORK_ERROR', message } };
   }
-
-  const { success, data } = await response.json();
-
-  return { success, data };
 }
